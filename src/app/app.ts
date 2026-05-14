728d5994fadb9b6ab840501659026eb36904b831
ChaitiDutta <simul.dutta.33@gmail.com>
2026-05-14 23:22:32 +0530

    repo List design created

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Userlist } from './shared/coponents/userlist/userlist';
+import { RepoList } from './shared/coponents/repo-list/repo-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Userlist],
+  imports: [RouterOutlet, Userlist, RepoList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

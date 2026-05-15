3e1c5d691fb5f2bc356a298bad6600bad11aba80
ChaitiDutta <simul.dutta.33@gmail.com>
2026-05-16 00:04:47 +0530

    Button created

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Userlist } from './shared/coponents/userlist/userlist';
 import { RepoList } from './shared/coponents/repo-list/repo-list';
+import { Button } from './shared/coponents/button/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Userlist, RepoList],
+  imports: [RouterOutlet, Userlist, RepoList, Button],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/shared/coponents/button/button.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+
+@Component({
+  selector: 'app-button',
+  imports: [MatButtonToggleModule],
+  templateUrl: './button.html',
+  styleUrl: './button.css',
+})
+export class Button {
+  userList(){}
+  repoList(){}
+}
